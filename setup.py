7d5aae907a1c53c646d547a2dd52e413fe6f0f73
Alvaro del Castillo <alvaro.delcastillo.contractor@bbva.com>
2019-04-18 12:05:37 +0200

    Add scripts for pip packaging

---
+++ setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 Alvaro del Castillo
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA.
+#
+# Authors:
+#     Alvaro del Castillo <alvaro.delcastillo@gmail.com>
+#
+
+import codecs
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme_md = os.path.join(here, 'README.md')
+
+# Pypi wants the description to be in reStrcuturedText, but
+# we have it in Markdown. So, let's convert formats.
+# Set up thinkgs so that if pypandoc is not installed, it
+# just issues a warning.
+try:
+    import pypandoc
+    README = pypandoc.convert(readme_md, 'rst')
+except (IOError, ImportError):
+    print("Warning: pypandoc module not found, or pandoc not installed. "
+          "Using md instead of rst")
+    with codecs.open(readme_md, encoding='utf-8') as f:
+        README = f.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='mydogbreed',
+    version='0.0.1',
+    packages=['dogbreed'],
+    include_package_data=True,
+    license='GPLv3',
+    description='MyDogBreed is a dog breed detection tool based on deep learning.',
+    long_description=README,
+    url='https://github.com/aylabs/mydogbreed',
+    author='Alvaro del Castillo',
+    author_email='alvaro.delcastillo@gmail.com',
+    keywords="development ai deep learning images dogs",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Environment :: X11 Applications :: Gnome',
+        'Environment :: Web Environment',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Image Recognition'
+    ],
+    install_requires=[
+    ],
+    scripts=[
+          'bin/mydogbreed'
+    ],
+    python_requires='>=3.6'
+)
